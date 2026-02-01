b94c1c824a2b46d3e2206810c5e69ecf3ecb6dd4
naufalalief <naufalalief087@gmail.com>
2026-02-01 17:00:14 +0700

    Refactor StartScreen widget layout to improve structure and readability

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -6,7 +6,29 @@ class StartScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Column(
-        children: [Image.asset('assets/images/quiz-logo.png', height: 300)],
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset('assets/images/quiz-logo.png', height: 300),
+          const SizedBox(height: 80),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+              foregroundColor: Colors.white,
+              textStyle: const TextStyle(fontSize: 20),
+            ),
+            child: const Text('Start Quiz'),
+          ),
+          const SizedBox(height: 30),
+        ],
       ),
     );
   }

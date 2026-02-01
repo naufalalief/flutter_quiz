9c483a5e28e196a2489c3ecb29ae2d5e61bb0807
naufalalief <naufalalief087@gmail.com>
2026-02-01 18:17:50 +0700

    Update StartScreen layout: adjust image properties and button structure for improved UI

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -8,7 +8,14 @@ class StartScreen extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Image.asset('assets/images/quiz-logo.png', height: 300),
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
+          ), // Opacity(
+          //   opacity: 0.6,
+          //   child: Image.asset('assets/images/quiz-logo.png', height: 300),
+          // ),
           const SizedBox(height: 80),
           const Text(
             'Learn Flutter the fun way!',
@@ -18,16 +25,17 @@ class StartScreen extends StatelessWidget {
               fontWeight: FontWeight.bold,
             ),
           ),
-          OutlinedButton(
+          const SizedBox(height: 30),
+          OutlinedButton.icon(
             onPressed: () {},
             style: OutlinedButton.styleFrom(
               padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
               foregroundColor: Colors.white,
               textStyle: const TextStyle(fontSize: 20),
             ),
-            child: const Text('Start Quiz'),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quiz'),
           ),
-          const SizedBox(height: 30),
         ],
       ),
     );
